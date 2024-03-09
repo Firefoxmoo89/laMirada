9087c8454939530cd9f464348e2c5e1d34706bee
firefoxmoo <firefoxmoo@gmail.com>
2024-03-08 17:59:16 -0700

    Mobile header css satisfied

--- script/footscript.js
+++ script/footscript.js
@@ -14,14 +14,14 @@ for (i=0;i<telInputList.length;i++) {
 		else if (theGuts.length>3 && theGuts.length<=6) {	part1 = theGuts.slice(0,3); part2 = theGuts.slice(3,-1)+theGuts.slice(-1)	}
 		else if (theGuts.length>6 && theGuts.length<=10) { part1 = theGuts.slice(0,3); part2 = theGuts.slice(3,6); part3 = theGuts.slice(6,-1)+theGuts.slice(-1) }
 		if (part1!="") { telInput.value = "("+part1 }
-		if (part2!="") { telInput.value += ") "+part2; }
-		if (part3!="") { telInput.value += "-"+part3; }
+		if (part2!="") { telInput.value += ") "+part2 }
+		if (part3!="") { telInput.value += "-"+part3 }
 	});
 }
 
 document.querySelector("button#mobileNav").addEventListener("click", () => { 
 	nav = document.querySelector("nav"); 
-	if (nav.style.display == "none") { nav.style.display = "flex" }
+	if (nav.style.display != "flex") { nav.style.display = "flex" }
 	else { nav.style.display = "none" }
 });
 
